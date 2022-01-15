18840fd7f85953fa0ac8f1fae94424124f6b1cff
dino <dinoalcatel@gmail.com>
2022-01-15 20:05:59 +0200

    Update

--- ImageBot/Program.cs
+++ ImageBot/Program.cs
@@ -1,19 +1,45 @@
-﻿using Newtonsoft.Json;
+﻿using Disboard;
+using Disboard.Mastodon;
+using Disboard.Mastodon.Enums;
+using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ImageBot
 {
+    enum AcceptedInput
+    {
+        Empty,
+        NoSpaces,
+        Url
+    }
+
     class Program
     {
         private static string _configFile = "settings.json";
         private static Config _config;
 
+        // # Client
+        private static MastodonClient _client = null;
+        private static AccessScope _scopes = AccessScope.Read | AccessScope.Write;
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            IsConfigured();
+            DeleteConfigFile();
+
+            if (IsConfigured())
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Bot have not been configured.");
+                SetupAsync().Wait();
+            }
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
@@ -42,7 +68,7 @@ namespace ImageBot
                 string json = File.ReadAllText(_configFile);
                 config = JsonConvert.DeserializeObject<Config>(json);
             }
-            catch (IOException ex)
+            catch (IOException)
             {
                 Console.WriteLine("Error: Could not open file.");
             }
@@ -50,9 +76,166 @@ namespace ImageBot
             return config;
         }
 
-        private static void Setup()
+        private static async Task SetupAsync()
         {
+            Console.WriteLine("Starting initial setup. Follow the instructions to setup the bot.");
+            _config = new Config();
 
+            // Instance Url
+            Console.WriteLine();
+            ///_config.Instance = GetInstanceUrl();
+
+            // Application Name
+            Console.WriteLine();
+            ///_config.ApplicationName = GetApplicationName();
+
+
+            // Register application
+            _client = new MastodonClient(_config.Instance);
+
+            await RegisterApplication();
+
+            // Auth user
+
+
+            Console.WriteLine("registered");
+            Console.WriteLine(_client.ClientSecret);
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(_config);
+                File.WriteAllText(_configFile, json);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error: Failed to save configuration to file.");
+            }
+        }
+
+        private static async Task RegisterApplication()
+        {
+            int retry = 3;
+            bool errors = true;
+            do
+            {
+                try
+                {
+                    if (retry < 3)
+                    {
+                        Console.WriteLine("Retrying...");
+                    }
+
+                    await _client.Apps.RegisterAsync(_config.ApplicationName, Constants.RedirectUriForClient, _scopes);
+                    errors = false;
+
+                }
+                catch (System.Net.Http.HttpRequestException)
+                {
+                    Console.WriteLine("Error: Could not connect to instance server.");
+                    retry -= 1;
+
+                    // Try again
+                    if (retry > 0)
+                    {
+                        ConsoleKey key;
+                        do
+                        {
+                            Console.WriteLine("Try again? (Y/n): ");
+                            key = Console.ReadKey().Key;
+                            Console.WriteLine();
+
+                            if (key == ConsoleKey.Y)
+                            {
+                                break;
+                            }
+                            else if (key == ConsoleKey.N)
+                            {
+                                // TODO: Exit
+                                Console.WriteLine("Exiting.");
+                            }
+
+                        } while (true);
+                    }
+                }
+            } while (errors && retry > 0);
+
+            if (retry == 0)
+            {
+                // TODO: exit
+                Console.WriteLine("Exiting.");
+            }
+        }
+
+        private static string GetInstanceUrl()
+        {
+            string input = string.Empty;
+            bool valid = true;
+            do
+            {
+                if (valid)
+                {
+                    Console.Write("Enter instance url (Example: mastodon.social): ");
+                }
+                else
+                {
+                    Console.Write("Invalid input. Enter application name (Default: ImageBot): ");
+                }
+
+                input = Console.ReadLine();
+
+
+                // Format
+                input = input.ToLower();
+                input = input.Replace("http://", "");
+                input = input.Replace("https://", "");
+
+                // Validate
+                if (!string.IsNullOrEmpty(input))
+                {
+                    // TODO: and regex valid.
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+
+            } while (!valid);
+
+
+            return input;
+        }
+
+        private static string GetApplicationName()
+        {
+            string input = string.Empty;
+            bool valid = true;
+            do
+            {
+                if (valid)
+                {
+                    Console.Write("Enter application name (Default: ImageBot): ");
+                }
+                else
+                {
+                    Console.Write("Invalid input. Enter application name (Default: ImageBot): ");
+                }
+
+                input = Console.ReadLine();
+
+                // Validate
+                if (string.IsNullOrEmpty(input))
+                {
+                    valid = true;
+                } // TODO: else if regex
+                else
+                {
+                    valid = false;
+                }
+
+            } while (!valid);
+
+            return input;
         }
 
         private static void DeleteConfigFile()
@@ -67,5 +250,7 @@ namespace ImageBot
     class Config
     {
         public bool IsConfigured { get; set; }
+        public string Instance { get; set; } = "mstdn.jp";
+        public string ApplicationName { get; set; } = "ImageBot";
     }
 }
